99f732c394968abb5a01dfb2b0eb098c6f878eb6
ohad.duering <ohadduering@gmail.com>
2020-09-23 19:19:30 +0200

    change admin screens to the brand color scheme

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'formatted_price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', )
+    list_editable = ('is_published', )
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+    def formatted_price(self, obj):
+        # obj is the Model instance
+        return '{:,.2f}'.format(obj.price)
+        
+admin.site.register(Listing, ListingAdmin)
+
