fe92ab3707f55e31525464f73c82f5e5c608fde4
ohad.duering <ohadduering@gmail.com>
2020-09-23 19:21:32 +0200

    add models for realtors and listings

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-def listings(request):
-    return render(request, 'listings/listings.html')
+from.models import Listing
+
+def index(request):
+    listings = Listing.objects.all
+    context = { 
+        'listings': listings 
+    }
+    return render(request, 'listings/listings.html', )
 
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
